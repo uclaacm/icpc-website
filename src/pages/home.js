df6c533849cfc6adf00db4ca8c5152be6a594dab
gu-ana <52898059+gu-ana@users.noreply.github.com>
2021-09-20 16:08:59 -0700

    Added Additional Officer bios. Condensed who we are and what we do into 1. Moved upcoming events towards the top of home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -53,13 +53,26 @@ const HomeContainer = () => {
           />
         )}
       </Box>
+      <Container >
+        <Stack p={3} spacing={3} align="center">
+          <Heading as="h1" fontSize={['2xl', '3xl']}>Upcoming Events</Heading>
+          <UpcomingEvents />
+        </Stack>
+      </Container>
       <Container narrow>
         <Stack p={3} spacing={3} align="center">
           <Heading as="h1" fontSize={['2xl', '3xl']}>who we are</Heading>
           <Heading as="h2" textAlign="center" fontSize={['md', 'lg']}>algorithmic thinking, problem solving</Heading>
           <Text textAlign="justify" fontSize={['sm', 'md']}>
-            We are a group of problem solvers at heart! ACM ICPC at UCLA is a student-led organization whose mission is to promote algorithmic thinking and help build proficient problem solvers. While we mostly dabble in competitive programming, we have grown to encompass algorithmic and creative thinking in general whether that helps you in answering those challenging coding interview questions or become a smarter and better programmer. We work diligently toward the shared goal of spreading the love of competitive programming and creative thinking to the greater UCLA community. We are also connected to the ICPC competition. Click below to find out more about the event.
+            We are a group of problem solvers at heart! ACM ICPC at UCLA is a student-led organization whose mission is to promote algorithmic thinking and help build proficient problem solvers.
+            While we mostly dabble in competitive programming, we have grown to encompass algorithmic and creative thinking in general whether that helps you in answering those challenging 
+            coding interview questions or become a smarter and better programmer. We work diligently toward the shared goal of spreading the love of competitive programming and creative thinking
+            to the greater UCLA community. From weekly workshops on competitive programming and technical interview preparation, to our own in-house puzzle hunts and beginner-friendly
+            programming contests, everything we do revolves around the central theme of critical algorithmic thinking.
           </Text>
+          <Text textalign="center" fontSize={['sm', 'md']}>
+          Click below to find out more about us:
+            </Text>
           <Flex wrap="wrap" justify="center" align="center">
             <Link to="team">
               <Button m={3} variantColor="brand" variant="outline">Meet Our Team</Button>
@@ -67,50 +80,18 @@ const HomeContainer = () => {
             <Link to="icpc">
               <Button m={3} variantColor="brand" variant="outline">ICPC Regionals</Button>
             </Link>
-          </Flex>
-        </Stack>
-      </Container>
-      <Divider p={3} borderColor="brand.500" />
-      <Container narrow>
-        <Stack p={3} spacing={3} align="center">
-          <Heading as="h1" fontSize={['2xl', '3xl']}>what we do</Heading>
-          <Text textAlign="justify" fontSize={['sm', 'md']}>
-            We are rooted in creative problem solving. From weekly workshops on competitive programming and technical interview preparation, to our own in-house puzzle hunts and beginner-friendly programming contests, everything we do revolves around the central theme of critical algorithmic thinking. Regardless of whether you are a complete beginner to programming (or even CS!), or have multiple years of experience in competitive programming, we have something to help you grow! All you need is the passion/interest!</Text>
-            { /* If you would like to join us as an officer and help teach/organize any of our workshops or events, apply to ICPC through the general ACM application linked below! We look forward to seeing your application. */ }
-          <Stack isInline flexWrap="wrap" justify="center" p={4}>
-            <Flex wrap="wrap" justify="center" align="center">
-              <Link to={{
+            <Link to={{
                 pathname: 'events',
                 hash: '#all-events',
               }}
               >
                 <Button m={3} variantColor="brand" variant="outline">Learn More About our Events</Button>
-              </Link>
-              {/* <Button */}
-              {/*   m={3} */}
-              {/*   variantColor="brand" */}
-              {/*   variant="outline" */}
-              {/*   onClick={() => toast({ */}
-              {/*     title: 'Applications are not open.', */}
-              {/*     description: 'Hi, thanks for showing interest! Applications are not open yet (-_-;). Check back around Novemeber for the ACM Intern/Officer Application Form.', */}
-              {/*     status: 'error', */}
-              {/*     duration: 5000, */}
-              {/*     isClosable: true, */}
-              {/*   })} */}
-              {/* > */}
-              {/*   Join Us! */}
-              {/* </Button> */}
-            </Flex>
-          </Stack>
+            </Link>
+          </Flex>
         </Stack>
       </Container>
       <Divider p={3} borderColor="brand.500" />
-      <Container narrow>
-        <Stack p={3} spacing={3} align="center">
-          <Heading as="h1" fontSize={['2xl', '3xl']}>Upcoming Events</Heading>
-          <UpcomingEvents />
-        </Stack>
-      </Container>
+      
     </div>
   );
 };
