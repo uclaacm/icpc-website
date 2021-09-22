afff9f87863bbb822fb8d08b56089306ba44c1e6
Rohit Nema <entrohpy@icloud.com>
2021-09-22 14:38:25 -0700

    Remove duplicate upcoming events container

--- src/pages/home.js
+++ src/pages/home.js
@@ -53,13 +53,7 @@ const HomeContainer = () => {
           />
         )}
       </Box>
-      <Container >
-        <Stack p={3} spacing={3} align="center">
-          <Heading as="h1" fontSize={['2xl', '3xl']}>Upcoming Events</Heading>
-          <UpcomingEvents />
-        </Stack>
-      </Container>
-      <Container narrow>
+      <Container>
         <Stack p={3} spacing={3} align="center">
           <Heading as="h1" fontSize={['2xl', '3xl']}>Upcoming Events</Heading>
           <UpcomingEvents />
@@ -71,13 +65,13 @@ const HomeContainer = () => {
           <Heading as="h2" textAlign="center" fontSize={['md', 'lg']}>algorithmic thinking, problem solving</Heading>
           <Text textAlign="justify" fontSize={['sm', 'md']}>
             We are a group of problem solvers at heart! ACM ICPC at UCLA is a student-led organization whose mission is to promote algorithmic thinking and help build proficient problem solvers.
-            While we mostly dabble in competitive programming, we have grown to encompass algorithmic and creative thinking in general whether that helps you in answering those challenging 
+            While we mostly dabble in competitive programming, we have grown to encompass algorithmic and creative thinking in general whether that helps you in answering those challenging
             coding interview questions or become a smarter and better programmer. We work diligently toward the shared goal of spreading the love of competitive programming and creative thinking
             to the greater UCLA community. From weekly workshops on competitive programming and technical interview preparation, to our own in-house puzzle hunts and beginner-friendly
             programming contests, everything we do revolves around the central theme of critical algorithmic thinking.
           </Text>
           <Text textalign="center" fontSize={['sm', 'md']}>
-          Click below to find out more about us:
+            Click below to find out more about us:
             </Text>
           <Flex wrap="wrap" justify="center" align="center">
             <Link to="team">
@@ -87,17 +81,17 @@ const HomeContainer = () => {
               <Button m={3} variantColor="brand" variant="outline">ICPC Regionals</Button>
             </Link>
             <Link to={{
-                pathname: 'events',
-                hash: '#all-events',
-              }}
-              >
-                <Button m={3} variantColor="brand" variant="outline">Learn More About our Events</Button>
+              pathname: 'events',
+              hash: '#all-events',
+            }}
+            >
+              <Button m={3} variantColor="brand" variant="outline">Learn More About our Events</Button>
             </Link>
           </Flex>
         </Stack>
       </Container>
       <Divider p={3} borderColor="brand.500" />
-      
+
     </div>
   );
 };
