e572d9d0f32d31db9971066b24bddaf861f008f0
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2022-04-29 22:56:31 -0700

    change admin password

--- src/pages/login.js
+++ src/pages/login.js
@@ -32,7 +32,7 @@ const { createHash } = require('crypto');
 
 function LoginContainer(props) {
 
-    if (createHash('sha256').update(props.cred).digest('hex') === '9a8622ec4e63d21f81c2b7c51b10a722aab9436f69d12d49e84e3918abe6b83a') {
+    if (createHash('sha256').update(props.cred).digest('hex') === '749f09bade8aca755660eeb17792da880218d4fbdc4e25fbec279d7fe9f65d70') {
         return <Redirect to="/update" />;
     } else {
         return (
