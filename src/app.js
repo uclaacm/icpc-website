f3635f43645a715d525a0dc4d5637a547e2bf38a
rathull <notifyrathul@gmail.com>
2025-10-18 12:15:04 -0400

    fix(discord): remove temporary discord invite link and replace with official

--- src/app.js
+++ src/app.js
@@ -205,7 +205,7 @@ const Footer = () => (
             <Link href="https://www.facebook.com/groups/ucla.icpc" isExternal>
               <Box as={FaFacebook} w={8} h={8} />
             </Link>
-            <Link href="https://discord.gg/jPXmBEWDgv" isExternal>
+            <Link href="https://discord.gg/6pxWhttfs6" isExternal>
               <Box as={FaDiscord} w={8} h={8} />
             </Link>
             <Link href="https://www.instagram.com/acm.ucla" isExternal>
