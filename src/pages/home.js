b2e5662ab1a01de76d7a8164dbf9c5457327301b
Prabhvir Babra <theprabab@gmail.com>
2026-03-22 16:48:11 -0700

    Fix sponsors (add X-camp + fix Citadel)

--- src/pages/home.js
+++ src/pages/home.js
@@ -106,7 +106,8 @@ const HomeContainer = () => {
             <Sponsor sponsor_name="Jane Street" external_link="https://www.janestreet.com/" img_src="static/sponsors/Jane_Street_Capital_Logo.svg" />
             <Sponsor sponsor_name="Traversal" external_link="https://www.traversal.com/" img_src="static/sponsors/traversal-full-logo.png" />
             <Sponsor sponsor_name="HRT" external_link="https://www.hudsonrivertrading.com/" img_src="static/sponsors/hrt-full-logo.png" />
-            <Sponsor sponsor_name="Citadel" external_link="https://www.citadel.com/" img_src="static/sponsors/Citadel-Logo.jpg" />
+            <Sponsor sponsor_name="Citadel" external_link="https://www.citadel.com/" img_src="static/sponsors/citadel.webp" />
+            <Sponsor sponsor_name="X-Camp" external_link="https://x-camp.academy/" img_src="static/sponsors/x-camp.webp" />
           </Flex>
         </Stack>
         <br/>      
